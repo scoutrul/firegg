64801b78a6d70c1fcae4f3d2a69c313f0072f234
Anton Golova <scoutrul@mail.ru>
2017-09-25 14:53:04 +0300

    hero bg changed

--- src/components/pages/blocks/IndexHero.js
+++ src/components/pages/blocks/IndexHero.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// <TitleBlock title={props.title11} />
+// <TitleBlock title={props.title12} />
+
 const TitleBlock = (props) => (
 	<div>
 		<span>
@@ -11,11 +14,10 @@ const TitleBlock = (props) => (
 export const IndexHero = (props) => {
 	return(
 		<section className="hero" style={bgGradient}>
+			<h1 style={bgHeader}>Guardsman</h1>
 			<div className="container" style={bgImage}>
 				<div className="screener">
 					<div className="title slide_up_text">
-						<TitleBlock title={props.title11} />
-						<TitleBlock title={props.title12} />
 						<div className="takecontrol">
 							<span>
 								{props.title13}
@@ -33,12 +35,29 @@ export const IndexHero = (props) => {
 		</section>
 	)
 }
+
+const bgHeader = {
+	position: "absolute",
+	width: "100%",
+	textAlign: "center",
+	color: "#FFF",
+	textTransform: "uppercase",
+	fontWeight: "900",
+	top: "30%",
+	fontSize: "13vw"
+}
+
 const bgGradient = {
 	background: "-webkit-linear-gradient(left, rgb(18,18,18) 0%,rgb(40,43,50) 15%,rgb(61,68,81) 50%,rgb(40,43,50) 85%, rgb(18,18,18) 100%)",
 }
 const bgImage = {
-	background: "url(./firegg/public/images/guardsman_promo.png) no-repeat",
+	background: "url(./images/guardsman_promo.png) no-repeat",
 	backgroundSize: "cover",
 	margin: "auto auto 0",
-	maxWidth: "1500px"
+	maxWidth: "1500px",
+	backgroundPositionY: "bottom",
+	backgroundPositionX: "center",
+	minHeight: "100vh"
 }
+
+	// background: "url(./firegg/public/images/guardsman_promo.png) no-repeat"
