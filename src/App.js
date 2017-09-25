609811b580717176efc3f92604cc22365bba2da6
Anton Golova <scoutrul@mail.ru>
2017-09-25 03:27:16 +0300

    new bg promo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import firebase from './fire'
+// import firebase from './fire'
 
 import Helmet from 'react-helmet'
 
 import Routes from './components/routes'
 import {HeaderMenu, MobileMenu, Hamburger, Locations, Footer} from './components/'
 
-const database = firebase.database()
+// const database = firebase.database()
 
-console.log(database)
-
-var query = database.ref("menu").orderByKey();
-query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      // key will be "ada" the first time and "alan" the second time
-      var key = childSnapshot.key;
-      // childData will be the actual contents of the child
-      var childData = childSnapshot.val();
-  });
-});
-      console.log(query)
+// var query = database.ref("menu").orderByKey();
+// query.once("value")
+//   .then(function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       // key will be "ada" the first time and "alan" the second time
+//       var key = childSnapshot.key;
+//       // childData will be the actual contents of the child
+//       var childData = childSnapshot.val();
+//   });
+// });
+// console.log(query)
 
 
 const mapStateToProps = state => {
--- src/components/pages/blocks/IndexHero.js
+++ src/components/pages/blocks/IndexHero.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const TitleBlock = (props) => (
+	<div>
+		<span>
+			{props.title}
+		</span>
+	</div>
+	)
 export const IndexHero = (props) => {
 	return(
-		<section className="hero">
-			<div className="container">
+		<section className="hero" style={bgGradient}>
+			<div className="container" style={bgImage}>
 				<div className="screener">
 					<div className="title slide_up_text">
-						<div>
-							<span>
-								{props.title11}
-							</span>
-						</div>
-						<div>
-							<span>
-								{props.title12}
-							</span>
-						</div>
+						<TitleBlock title={props.title11} />
+						<TitleBlock title={props.title12} />
 						<div className="takecontrol">
 							<span>
 								{props.title13}
@@ -33,4 +32,13 @@ export const IndexHero = (props) => {
 		 	</div>
 		</section>
 	)
+}
+const bgGradient = {
+	background: "-webkit-linear-gradient(left, rgb(18,18,18) 0%,rgb(40,43,50) 15%,rgb(61,68,81) 50%,rgb(40,43,50) 85%, rgb(18,18,18) 100%)",
+}
+const bgImage = {
+	background: "url(./images/index_hat.png) no-repeat",
+	backgroundSize: "cover",
+	margin: "auto auto 0",
+	maxWidth: "1500px"
 }
