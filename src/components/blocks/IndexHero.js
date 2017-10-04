68cebc2913f4de38ed99eb1389988af6d2aa8b6b
Anton Golova <scoutrul@mail.ru>
2017-10-04 16:13:18 +0300

    Main screen parallaxed and smooth scrolled

--- src/components/blocks/IndexHero.js
+++ src/components/blocks/IndexHero.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
+import ReactSmoothScroll from 'react-smooth-scroll'
+
 
 export const IndexHero = ({title11, title12, title1link}) => {
 	return(
-		<section className="hero">
-			<h1 className="promo">{title11}</h1>
-			<div className="container">
-				<div className="screener">
-					<div className="title slide_up_text">
-						<div className="takecontrol">
-							<span>
-								{title12}
-							</span>
-						</div>
+		<ReactSmoothScroll>
+			<section className="hero">
+				<ParallaxProvider>
+					<Parallax
+						className="promo"
+						offsetYMax={150}
+						offsetYMin={-150}
+						slowerScrollRate
+						tag="h1">
+						{title11}
+					</Parallax>
+
+					<div className="container">
+						<div className="screener">
+							<Parallax
+								className="title slide_up_text"
+								offsetYMax={20}
+								offsetYMin={-20}
+								slowerScrollRate
+								tag="div">
+								<div className="takecontrol">
+									<span>
+										{title12}
+									</span>
+
+								</div>
+
+							<button className="center red">
+								<Link className="button_inner" to="/products">
+									<div className="arrows"></div>
+									<div className="message">{title1link}</div> 
+								</Link>
+							</button>
+							</Parallax>
+					 	</div>
 					</div>
-					<button className="center red">
-						<Link className="button_inner" to="/products">
-							<div className="arrows"></div>
-							<div className="message">{title1link}</div> 
-						</Link>
-					</button>
-			 	</div>
-		 	</div>
-		</section>
+				 </ParallaxProvider>
+			</section>
+		</ReactSmoothScroll>
 	)
 }
 
