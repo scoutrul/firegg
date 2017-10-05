d6ae6f13947e4fb5e06a63296d8b3595edc228d9
Anton Golova <scoutrul@mail.ru>
2017-10-05 06:04:30 +0300

    appearModule without jQuery

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { ParallaxController } from 'react-scroll-parallax'
-
+import { ParallaxProvider } from 'react-scroll-parallax'
 import Helmet from 'react-helmet'
 
 import Routes from './components/routes'
 import {Header, Footer, Wrapper} from './components/blocks'
-import appearModule from './js/appearModule'
+import './js/appearModule'
 
 
-
-//Console log title
-console.log("%cGuardsman Limited", "font: 2.2rem 'Work Sans', 'sans-serif'; color: #FFF; background-color: #1b1b1b; text-decoration: underline #ff002e");
-
-
-//magic appear//////////
-window.onload = function() {
-	(document.visibilityState === 'visible') && setTimeout(appearModule.addToAllClasses, 500);
-};
-
-("focus resize scroll click mousemove keydown".split(" ")).forEach(function(e){
-   window.addEventListener(e,appearModule.addToAllClasses,false);
-});
-////////////////////////
 
 const mapStateToProps = state => {
 	return {
@@ -32,21 +17,22 @@ const mapStateToProps = state => {
 	}
 }
 
-ParallaxController.init() 
 const App = (props) => {
 
 	const siteMenu = props.menuItems
 	const socialMenu = props.socialItems
 	return (
-		<div id="app" style={styles}>
-			<Helmet title="Guardsman Limited®" />
+		<ParallaxProvider>
+			<div id="app" style={styles}>
+				<Helmet title="Guardsman Limited®" />
 
-			<Wrapper MenuItems={siteMenu}>
-				<Header MenuItems={siteMenu}/>
-				<Routes/>
-				<Footer MenuItems={siteMenu} SocialItems={socialMenu}/>
-			</Wrapper>
-		</div>
+				<Wrapper MenuItems={siteMenu}>
+					<Header MenuItems={siteMenu}/>
+					<Routes/>
+					<Footer MenuItems={siteMenu} SocialItems={socialMenu}/>
+				</Wrapper>
+			</div>
+		</ParallaxProvider>
 	)		
 	
 }
--- src/js/appearModule.js
+++ src/js/appearModule.js
@@ -1,22 +1,36 @@
-import $ from 'jquery'
 
 const appearModule = {
 	    addToAllClasses: function() {
-	        $('.slide_up_text').each(function(i, el) {
-	            (appearModule.isScrolledIntoView(el)) && $(this).addClass('slide_up_text--active') // toggle: || $(this).removeClass('slide_up_text--active');
+	        Array.from(document.getElementsByClassName('slide_up_text')).forEach(function(el) {
+	            (appearModule.isScrolledIntoView(el)) && el.classList.add('slide_up_text--active') // toggle: || $(this).removeClass('slide_up_text--active');
 	        })
 	    },
 	    isScrolledIntoView: function(elem) {
-	        var viewTop = $(window).scrollTop();
-	        var viewBottom = viewTop + $(window).height();
-	        var elemTop = $(elem).offset().top;
-	        var elemBottom = elemTop + $(elem).height();
+	        var viewTop = window.pageYOffset || document.documentElement.scrollTop;
+	        var viewBottom = viewTop + window.innerHeight;
+
+	        var elemTop = elem.offsetTop;
+
+	        var elemBottom = elemTop + elem.getBoundingClientRect().height;
+
 	        if (window.innerWidth < 400) {
 	            return true
-	            // mobile width detect
 	        }
 	        return ((elemBottom <= viewBottom) && (elemTop >= viewTop))
 	    }
 	};
 	
-export default appearModule 
+
+//Console log title
+console.log("%cGuardsman Limited", "font: 2.2rem 'Work Sans', 'sans-serif'; color: #FFF; background-color: #1b1b1b; text-decoration: underline #ff002e");
+
+
+//magic appear//////////
+window.onload = function() {
+	(document.visibilityState === 'visible') && setTimeout(appearModule.addToAllClasses, 500);
+};
+
+("focus resize scroll click mousemove keydown".split(" ")).forEach(function(e){
+   window.addEventListener(e,appearModule.addToAllClasses,false);
+});
+////////////////////////
